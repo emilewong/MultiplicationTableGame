f0d196d49e405b5ec469427911914ad6fde2dc5e
Emile Wong <emilewong@outlook.com>
2021-05-31 19:43:45 +0800

    Initial Commit

---
+++ MultiplicationTableGame/MultiplicationTableGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTableGameApp.swift
+//  MultiplicationTableGame
+//
+//  Created by Emile Wong on 31/5/2021.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTableGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
